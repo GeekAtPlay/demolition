92e2a39fadd35eb93d65d14f5349fa44f5bd7970
jasonb22 <jason.buchanan@us.usana.com>
2015-05-30 18:33:40 -0600

    Adding cors Filter.

--- src/main/java/com/geekatplay/demolition/Application.java
+++ src/main/java/com/geekatplay/demolition/Application.java
@@ -1,15 +1,60 @@
 package com.geekatplay.demolition;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.embedded.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpStatus;
 
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 import java.util.Arrays;
+
+
+//
+// curl -X POST -vu android-vehicles:123456 http://localhost:8080/oauth/token -H "Accept: application/json" -d "password=password&username=jason&grant_type=password&scope=write&client_secret=123456&client_id=android-vehicles"
+// curl -v POST http://127.0.0.1:8080/tags --data "tags=cows,dogs"  -H "Authorization: Bearer 66953496-fc5b-44d0-9210-b0521863ffcb"
+
 
 @SpringBootApplication
 public class Application {
 
+
+    @Bean
+    FilterRegistrationBean corsFilter(@Value("${tagit.origin:http://localhost:9000}") String origin) {
+        return new FilterRegistrationBean(new Filter(){
+
+            @Override
+            public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
+                HttpServletRequest request = (HttpServletRequest) req;
+                HttpServletResponse response = (HttpServletResponse) res;
+
+                String method = request.getMethod();
+
+                // this origin value could just as easily have come from a database
+                response.setHeader("Access-Control-Allow-Origin", origin);
+                response.setHeader("Access-Control-Allow-Methods", "POST,GET,OPTIONS,DELETE");
+                response.setHeader("Access-Control-Max-Age", Long.toString(60 * 60));
+                response.setHeader("Access-Control-Allow-Credentials", "true");
+                response.setHeader("Access-Control-Allow-Headers", "Origin,Accept,X-Requested-With,Content-Type,Access-Control-Request-Method,Access-Control-Request-Headers,Authorization");
+                if("OPTIONS".equals(method)) {
+                    response.setStatus(HttpStatus.OK.value());
+                } else {
+                    chain.doFilter(req, res);
+                }
+            }
+
+            @Override
+            public void init(FilterConfig filterConfig) throws ServletException { }
+
+            @Override
+            public void destroy() { }
+        });
+    }
 
     @Bean
     CommandLineRunner init(AccountRepository accountRepository,
